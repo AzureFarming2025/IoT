a60567fcb66819f5279f8049b4713509cde65eaa
Peegandourado7 <86914647+Peegandourado7@users.noreply.github.com>
2025-03-15 16:23:35 +0530

    bug/test: esp32+dht (virtual simulation)
    loading is pending

--- azurefarming/main.py
+++ azurefarming/main.py
@@ -21,3 +21,28 @@ print("Hello, ESP32!")
 #     oled.show()
 
 #     time.sleep(1)  # Delay before next reading cycle
+
+from machine import Pin
+from time import sleep
+import dht 
+
+led = Pin(19, Pin.OUT)
+sensor = dht.DHT22(Pin(32))
+
+while True:
+   
+    try:
+        sleep(2)
+        sensor.measure()
+        temp = sensor.temperature()
+        if temp > 25.0:
+            led.value(1)
+        else:
+            led.value(0)
+        hum = sensor.humidity()
+        temp_f = temp * (9/5) + 32.0
+        print('Temperature: %3.1f C' %temp)
+        print('Temperature: %3.1f F' %temp_f)
+        print('Humidity: %3.1f %%' %hum)
+    except OSError as e:
+        print('Failed to read sensor.')
