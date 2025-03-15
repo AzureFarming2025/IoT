3ca1ddb835ecf6fd83a37bf0ee5e3724f80bcdfd
Peegandourado7 <86914647+Peegandourado7@users.noreply.github.com>
2025-03-15 15:46:55 +0530

    chore : add network config & SaS token (personal)

--- iot/config.py
+++ iot/config.py
@@ -1,8 +1,8 @@
 # Wi-Fi Credentials
-WIFI_SSID = "DLive_5F4A"
-WIFI_PASSWORD = "62CEED5F49"
+WIFI_SSID = "Galaxy A33 5G 6770"
+WIFI_PASSWORD = ""
 
 # Azure IoT Hub Credentials
 IOT_HUB_NAME = "azurefarming-iothub"
 IOT_HUB_DEVICE_ID = "esp32-1"
-IOT_HUB_SAS_TOKEN = "SharedAccessSignature sr=azurefarming-iothub.azure-devices.net&sig=K4K9IHsgYF0CxFJoyDztErVibSnf5EmTm5pGBaDWrPo%3D&skn=iothubowner&se=1741964398"
+IOT_HUB_SAS_TOKEN = "SharedAccessSignature sr=azurefarming-iothub.azure-devices.net&sig=YFPKtDMsoJrNzc2fLK5xmYZmzpxPJY2c4MHIExvs7Js%3D&skn=iothubowner&se=1741907226"
