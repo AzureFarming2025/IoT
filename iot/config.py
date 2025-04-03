a9bdcfe39094a42d91bbd44f24a1d78d9517951b
Peegandourado7 <86914647+Peegandourado7@users.noreply.github.com>
2025-04-03 18:46:35 +0530

    changed

--- iot/config.py
+++ iot/config.py
@@ -5,4 +5,4 @@ WIFI_PASSWORD = ""
 # Azure IoT Hub Credentials
 IOT_HUB_NAME = "YourIoTHub"
 IOT_HUB_DEVICE_ID = "your_device"
-IOT_HUB_SAS_TOKEN = None
+IOT_HUB_SAS_TOKEN = "SharedAccessSignature sr=azurefarming-iothub.azure-devices.net%2Fdevices%2Fesp32-2&sig=KKP809nfqUXwoy6G096xmEfNUl%2FQQKcBdI3hwaQDqPc%3D&se=1743687676
